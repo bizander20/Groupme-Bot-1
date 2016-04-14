88ca1da4056fabf1f92bf869769d6b02d968836b
bizander20 <beanderson0503@hotmail.com>
2016-04-14 14:53:30 -0400

    Update time.js

--- custom_commands/time.js
+++ custom_commands/time.js
@@ -1,6 +1,4 @@
-exports.modName = "Get Time"
-
-function (registerCommand) {
+module.exports = function (registerCommand) {
   registerCommand('time', 'time: Get the current time', function (groupLocalID, userDisplayName, msgTokens, callback) {
     callback('The current time is ' + (new Date()).toString());
   });
